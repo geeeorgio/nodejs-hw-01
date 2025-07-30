2d229bf783e5266debc57afec54541feae8fb866
georgiy <heorhii.kyryliuk@gmail.com>
2025-07-30 20:11:47 +0300

    done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const prevContacts = await readContacts();
+  const newContacts = Array(number).fill(null).map(createFakeContact);
+
+  await writeContacts([...prevContacts, ...newContacts]);
+};
 
 generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,14 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contactsList = await readContacts();
+
+  if (contactsList.length > 0) {
+    contactsList.pop();
+  }
+
+  await writeContacts(contactsList);
+};
 
 removeLastContact();
